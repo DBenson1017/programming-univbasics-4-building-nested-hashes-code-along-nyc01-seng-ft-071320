3a0ab7be6f9efb25bf201519bb0160b634e9749e
Devin Benson <devinbenson@gmail.com>
2020-06-26 20:12:41 +0000

    Automatically backed up by Learn

--- lib/02_second_challenge.rb
+++ lib/02_second_challenge.rb
@@ -15,3 +15,21 @@ def second_challenge
 }
   
 end
+
+
+  epic_tragedy = {
+    :montague => {
+      :patriarch => "Lord Montague" ,
+      :matriarch => "Lady Montague" ,
+      :hero => "Romeo",
+      :hero_friends => ["Benvollo", "Mercutio"] 
+      }, 
+    :capulet => {
+      :patriarch => "Lord Capulet",
+      :matriarch => "Lady Capulet",
+      :heroine => "Juliet" ,
+      :heroine_friends => ["Steven", "Nurse"]
+      }
+}
+  
+end
