9baba5309d31f4ef3c411c52abdfd59216bb5c51
Devin Benson <devinbenson@gmail.com>
2020-06-26 20:14:15 +0000

    Automatically backed up by Learn

--- lib/02_second_challenge.rb
+++ lib/02_second_challenge.rb
@@ -1,15 +1,15 @@
 def second_challenge
   epic_tragedy = {
     :montague => {
-      :patriarch => "" ,
-      :matriarch => "" ,
-      :hero => "",
+      :patriarch => ,
+      :matriarch =>  ,
+      :hero => ,
       :hero_friends => [] 
       }, 
     :capulet => {
-      :patriarch => "",
-      :matriarch => "",
-      :heroine => "" ,
+      :patriarch => ,
+      :matriarch => ,
+      :heroine =>  ,
       :heroine_friends => []
       }
 }
@@ -32,4 +32,3 @@ end
       }
 }
   
-end
